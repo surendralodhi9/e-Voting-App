9c9c64f7d8b512da546b810f459108727179707f
Surendra Lodhi <surendralodhi9@gmail.com>
2020-07-12 15:58:24 +0530

    First commit

---
+++ eVotingApp/app/src/main/java/com/example/evotingapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.evotingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    public ImageButton imageButtonAdmin;
+    public ImageButton imageButtonLogin;
+    public ImageButton imageButtonAbout;
+    public ImageButton imageButtonHelp;
+    public ImageButton imageButtonContact;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        setUpAllUi();
+
+        imageButtonAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),AdminActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    protected void setUpAllUi()
+    {
+
+        imageButtonAdmin=(ImageButton)findViewById(R.id.imageButtonAdmin);
+        imageButtonLogin=(ImageButton)findViewById(R.id.imageButtonLogin);
+        imageButtonAbout=(ImageButton)findViewById(R.id.imageButtonAbout);
+        imageButtonContact=(ImageButton)findViewById(R.id.imageButtonContact);
+        imageButtonHelp=(ImageButton)findViewById(R.id.imageButtonHelp);
+
+
+
+    }
+}
